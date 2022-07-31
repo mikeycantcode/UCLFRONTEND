7d037fe67884fb687402d0aed2de62189b6e0c6c
mike hakiz <66285507+mikeycantcode@users.noreply.github.com>
2022-07-31 18:18:13 -0400

    finished showing the data
    ...

--- js/beginLoans.js
+++ js/beginLoans.js
@@ -1,6 +1,4 @@
-//FILLED WITH LOCALHOST TESTING DATA RN
-
-
+let provider
 
 
 function truncateAddress(address) {
@@ -22,7 +20,7 @@ async function loginWithEth() {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     await provider.send("eth_requestAccounts", []);
     const signer = provider.getSigner()
-    let address = await signer.getAccount()
+    let address = await signer.getAddress()
     localStorage.setItem("userAddress", address)
     document.getElementById("loginButton").innerHTML = truncateAddress(address);
 
@@ -30,8 +28,6 @@ async function loginWithEth() {
     document.getElementById("loginButton").classList.remove("bg-red-500");
     document.getElementById("loginButton").classList.add("bg-orange-500");
 }
-
-
 
 let ucLoanABI = [
     {
@@ -402,208 +398,146 @@ let ucLoanABI = [
     }
 ]
 
-
-//check if window.ethereum detected
-function bonger() {
-    if (!window.ethereum) {
-        document.getElementById("alert-1").classList.remove("hidden")
-    } else {
-        if (localStorage.getItem("userAddress")) {
-            document.getElementById("loginButton").innerHTML = truncateAddress(localStorage.getItem("userAddress"))
-            getData()
-        }
+let factoryAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+let factoryABI = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_borrower",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "_guarantor",
+                "type": "address"
+            },
+            {
+                "internalType": "uint16",
+                "name": "_interestRate",
+                "type": "uint16"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_amountBorrowed",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_requiredCollateral",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_dueDate",
+                "type": "uint256"
+            }
+        ],
+        "name": "newLoan",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_address",
+                "type": "address"
+            }
+        ],
+        "name": "viewAddressOfBorrower2Loan",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_address",
+                "type": "address"
+            }
+        ],
+        "name": "viewAddressOfGuarantor2Loan",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_address",
+                "type": "address"
+            }
+        ],
+        "name": "viewAddressOfLender2Loan",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
     }
-}
+]
 
-//function that on load of the page gets the data from the blockchain async window
-async function getData() {
-    let loanData
-    let factoryAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-    let factoryABI = [
-        {
-            "inputs": [],
-            "stateMutability": "nonpayable",
-            "type": "constructor"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "address",
-                    "name": "_borrower",
-                    "type": "address"
-                },
-                {
-                    "internalType": "address",
-                    "name": "_guarantor",
-                    "type": "address"
-                },
-                {
-                    "internalType": "uint16",
-                    "name": "_interestRate",
-                    "type": "uint16"
-                },
-                {
-                    "internalType": "uint256",
-                    "name": "_amountBorrowed",
-                    "type": "uint256"
-                },
-                {
-                    "internalType": "uint256",
-                    "name": "_requiredCollateral",
-                    "type": "uint256"
-                },
-                {
-                    "internalType": "uint256",
-                    "name": "_dueDate",
-                    "type": "uint256"
-                }
-            ],
-            "name": "newLoan",
-            "outputs": [
-                {
-                    "internalType": "address",
-                    "name": "",
-                    "type": "address"
-                }
-            ],
-            "stateMutability": "payable",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "address",
-                    "name": "_address",
-                    "type": "address"
-                }
-            ],
-            "name": "viewAddressOfBorrower2Loan",
-            "outputs": [
-                {
-                    "internalType": "address",
-                    "name": "",
-                    "type": "address"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "address",
-                    "name": "_address",
-                    "type": "address"
-                }
-            ],
-            "name": "viewAddressOfGuarantor2Loan",
-            "outputs": [
-                {
-                    "internalType": "address",
-                    "name": "",
-                    "type": "address"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "address",
-                    "name": "_address",
-                    "type": "address"
-                }
-            ],
-            "name": "viewAddressOfLender2Loan",
-            "outputs": [
-                {
-                    "internalType": "address",
-                    "name": "",
-                    "type": "address"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        }
-    ]
-
+async function createLoan() {
+    //0x0000000000000000000000000000000000000000
+    const deactivateGuarantor = "0x2546BcD3c84621e976D8185a91A922aE77ECEc30"
+    var borrower = document.getElementById("borrower").value.toString()
+    console.log(borrower)
+    var amountToLend = ethers.utils.parseUnits(document.getElementById("amountToLend").value, 18).toString()
+    console.log(amountToLend)
+    var intRate = document.getElementById("intRate").value.toString()
+    console.log(intRate)
+    var reqCollateral = ethers.utils.parseUnits(document.getElementById("reqCollateral").value, 18).toString()
+    console.log(reqCollateral)
+    var dueDate = document.getElementById("dueDate").value.toString()
+    console.log(dueDate)
+    const options = { value: ethers.utils.parseEther("4.0") }
+    ethereum.request({ method: 'eth_requestAccounts' })
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
     const factoryContract = new ethers.Contract(factoryAddress, factoryABI, provider)
-
-
-    const addressOfLoan = await factoryContract.viewAddressOfBorrower2Loan("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
-    let check = isNullAddress(addressOfLoan, "borrowerLoans")
-    if (check) {
-        const currLoanContract = new ethers.Contract(addressOfLoan, ucLoanABI, provider)
-        if (await currLoanContract.isLoanActive()) {
-            loanData = await loanFound(addressOfLoan, currLoanContract, 1)
-        } else {
-
-        }
-    }
-
-
-    const addressOfLoan2 = await factoryContract.viewAddressOfLender2Loan("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
-    check = isNullAddress(addressOfLoan2, "lenderLoans")
-    if (check) {
-        const currLoanContract = new ethers.Contract(addressOfLoan, ucLoanABI, provider)
-        if (await currLoanContract.isLoanActive()) {
-            loanData = await loanFound(addressOfLoan2, currLoanContract, 2)
-        } else {
-            await pendingLoan(currLoanContract)
-        }
-    }
-
-
-
+    const connectedFactory = factoryContract.connect(signer);
+    const createLoan = await connectedFactory.newLoan(
+        borrower,
+        deactivateGuarantor,
+        intRate,
+        amountToLend.toString(),
+        reqCollateral,
+        dueDate,
+        options
+    )
+    console.log(createLoan)
+    const tx = await createLoan.wait()
+    console.log(tx)
 
 }
-
-//function to change the display if no loans found
-function noLoansFound(id) {
-    document.getElementById(id).innerHTML = "No Loans found. Start a new loan in the [Start a Loan] tab."
-}
-
-//function to test for null
-function isNullAddress(address, id) {
-    if (address == 0x0000000000000000000000000000000000000000) {
-        noLoansFound(id)
-        return false
-    } else {
-        return true
-    }
-}
-
-//function to change the data
-async function loanFound(address, contract, number) {
-    const loanAddress = address
-    console.log(loanAddress)
-    document.getElementById("loanAddress" + number).innerHTML = "Address of Loan : " + loanAddress
-    const lenderAddress = await contract.viewLender()
-    console.log(lenderAddress)
-    document.getElementById("lenderAddress" + number).innerHTML = "Address of Lender : " + lenderAddress
-    const borrowerAddress = await contract.viewBorrower()
-    console.log(borrowerAddress)
-    document.getElementById("borrowerAddress" + number).innerHTML = "Address of Borrower : " + borrowerAddress
-    const amountBorrowed = BigInt(await contract.amountBorrowed()).toString()
-    console.log(amountBorrowed)
-    document.getElementById("amountBorrowed" + number).innerHTML = "Amount Borrowed : " + amountBorrowed + " wei"
-    const amountToPay = BigInt(await contract.viewDebt()).toString()
-    console.log(amountToPay)
-    document.getElementById("amountTotalPay" + number).innerHTML = "Amount Left To Pay : " + amountToPay + " wei"
-    const timeLeft = await contract.viewTime()
-    console.log(timeLeft)
-}
-
-async function pendingLoan(contract) {
-    document.getElementById("pLoanText").innerHTML = "Loan from " + await contract.viewLender() + " for " + BigInt(await contract.amountBorrowed()).toString() + " wei"
-    var original = document.querySelector('#pLoanTemplate');
-    var clone = original.cloneNode(true);
-    clone.id = 'pLoan1'
-    clone.classList.remove('hidden');
-    original.after(clone);
-}
-
-
